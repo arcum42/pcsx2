d146137c4cb59b00881b70c41446d7bb943129aa
arcum42 <arcum42@gmail.com>
2018-08-20 00:20:54 -0700

    Still not configuring anything, but it has three tabs.

--- plugins/LilyPad/Linux/Dialog.cpp
+++ plugins/LilyPad/Linux/Dialog.cpp
@@ -50,29 +50,85 @@ void LinuxAboutDialog()
     SysMessage("Lilypad is a gamepad plugin for pcsx2 by ChickenLiver, ported to Linux by gregory, arcum42, and the PCSX2 Dev Team.");
 }
 
+GtkWidget *Create_General_Tab()
+{
+    GtkWidget *tab_box;
+    GtkWidget *tab_frame;
+    GtkWidget *label;
+
+    tab_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+	tab_frame = gtk_frame_new("General");
+    label = gtk_label_new("I am a configuration dialog box.");
+
+    gtk_container_add(GTK_CONTAINER(tab_frame), tab_box);
+    gtk_box_pack_start (GTK_BOX (tab_box), label, TRUE, FALSE, 0);
+
+    return tab_frame;
+}
+
+GtkWidget *Create_Controller_1_Tab()
+{
+    GtkWidget *tab_box;
+    GtkWidget *tab_frame;
+    GtkWidget *label;
+
+    tab_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+	tab_frame = gtk_frame_new("General");
+    label = gtk_label_new("I am the second page of a configuration dialog box.");
+
+    gtk_container_add(GTK_CONTAINER(tab_frame), tab_box);
+    gtk_box_pack_start (GTK_BOX (tab_box), label, TRUE, FALSE, 0);
+
+    return tab_frame;
+}
+
+GtkWidget *Create_Controller_2_Tab()
+{
+    GtkWidget *tab_box;
+    GtkWidget *tab_frame;
+    GtkWidget *label;
+
+    tab_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+	tab_frame = gtk_frame_new("General");
+    label = gtk_label_new("I am the third page of a configuration dialog box.");
+
+    gtk_container_add(GTK_CONTAINER(tab_frame), tab_box);
+    gtk_box_pack_start (GTK_BOX (tab_box), label, TRUE, FALSE, 0);
+
+    return tab_frame;
+}
+
 void ShowLinuxConfigDialog()
 {
-    //SysMessage("I am a configuration dialog box.");
     GtkWidget *dialog;
-    GtkWidget *label;
     GtkWidget *main_box, *main_frame;
+    GtkWidget *notebook;
+    GtkWidget *general_tab_frame, *cont1_tab_frame, *cont2_tab_frame;
+
     int result = 0;
 
     dialog = gtk_dialog_new_with_buttons ("Lilypad",
                                         NULL,
                                         (GtkDialogFlags)(GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT),
-                                        "_OK",
-                                        GTK_RESPONSE_ACCEPT,
                                         "_Cancel",
                                         GTK_RESPONSE_REJECT,
+                                        "_OK",
+                                        GTK_RESPONSE_ACCEPT,
                                         NULL);
 
     // Note: Currently just coding for Gtk 3. I'll worry about Gtk 2 later.
-    label = gtk_label_new("I am a configuration dialog box.");
+    notebook = gtk_notebook_new();
     main_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 	main_frame = gtk_frame_new("Lilypad Config");
+    general_tab_frame = Create_General_Tab();
+    cont1_tab_frame = Create_Controller_1_Tab();
+    cont2_tab_frame = Create_Controller_2_Tab();
 
-    gtk_box_pack_start (GTK_BOX (main_box), label, TRUE, FALSE, 0);
+    result = gtk_notebook_append_page_menu (GTK_NOTEBOOK(notebook), general_tab_frame, gtk_label_new("General"), NULL);
+    result = gtk_notebook_append_page_menu (GTK_NOTEBOOK(notebook), cont1_tab_frame, gtk_label_new("Controller 1"), NULL);
+    result = gtk_notebook_append_page_menu (GTK_NOTEBOOK(notebook), cont2_tab_frame, gtk_label_new("Controller 2"), NULL);
+
+    gtk_box_pack_start (GTK_BOX (main_box), notebook, TRUE, FALSE, 0);
     gtk_container_add(GTK_CONTAINER(main_frame), main_box);
     gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))), main_frame);
 
