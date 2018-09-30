16fd0aa02092cf4738491576a800c169fd046266
Shanoah Alkire <arcum42@gmail.com>
2018-09-29 19:31:22 -0700

    Add validation code, and a file I seemingly forgot to commit a while ago.

--- plugins/GSone/Vulkan/GSVulkan.cpp
+++ plugins/GSone/Vulkan/GSVulkan.cpp
@@ -19,10 +19,22 @@
 #include "GSVulkan.h"
 #include <vector>
 
+struct vulkan_context {
+								
+    uint32_t width;
+    uint32_t height;
+
+    VkInstance instance;
+};
+
+vulkan_context context;
 VkInstance vk_inst;
 
-bool CreateInstance()
+bool CreateInstance(bool use_layers)
 {
+    const char *layers[] = { "VK_LAYER_LUNARG_standard_validation" };
+    VkResult res;
+
     // initialize the VkApplicationInfo structure
     VkApplicationInfo app_info = {};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -41,12 +53,20 @@ bool CreateInstance()
     inst_info.pApplicationInfo = &app_info;
     inst_info.enabledExtensionCount = 0;
     inst_info.ppEnabledExtensionNames = NULL;
-    inst_info.enabledLayerCount = 0;
-    inst_info.ppEnabledLayerNames = NULL;
 
-    VkResult res;
+    if (use_layers)
+    {
+        inst_info.enabledLayerCount = 1;
+        inst_info.ppEnabledLayerNames = layers;
+    }
+    else
+    {
+        inst_info.enabledLayerCount = 0;
+        inst_info.ppEnabledLayerNames = NULL;
+    }
 
-    res = vkCreateInstance(&inst_info, NULL, &vk_inst);
+
+    res = vkCreateInstance(&inst_info, NULL, &context.instance);
 
     if (res == VK_ERROR_INCOMPATIBLE_DRIVER) 
     {
@@ -63,12 +83,48 @@ bool CreateInstance()
     return true;
 }
 
+void PrintDeviceInfo(VkPhysicalDevice physicalDevice)
+{
+    uint32_t queueFamilyCount = 0;
+    VkPhysicalDeviceProperties deviceProperties;
+    
+
+    memset(&deviceProperties, 0, sizeof deviceProperties);
+    vkGetPhysicalDeviceProperties(physicalDevice, &deviceProperties);
+
+    GSLog::WriteLn("Driver Version: %d", deviceProperties.driverVersion);
+    GSLog::WriteLn("Device Name:    %s", deviceProperties.deviceName);
+    GSLog::WriteLn("Device Type:    %d", deviceProperties.deviceType);
+    GSLog::WriteLn("API Version:    %d.%d.%d",
+        VK_VER_MAJOR(deviceProperties.apiVersion),
+        VK_VER_MINOR(deviceProperties.apiVersion),
+        VK_VER_PATCH(deviceProperties.apiVersion));
+
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, NULL);
+
+    std::vector<VkQueueFamilyProperties>familyProperties(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, &familyProperties[0]);
+
+    // Print the families
+    for (u32 j = 0; j < queueFamilyCount; j++) 
+    {
+        GSLog::WriteLn("Count of Queues: %d", familyProperties[j].queueCount);
+        GSLog::WriteLn("Supported operations on this queue:");
+
+        if (familyProperties[j].queueFlags & VK_QUEUE_GRAPHICS_BIT) GSLog::WriteLn("\t\t Graphics");
+        if (familyProperties[j].queueFlags & VK_QUEUE_COMPUTE_BIT) GSLog::WriteLn("\t\t Compute");
+        if (familyProperties[j].queueFlags & VK_QUEUE_TRANSFER_BIT) GSLog::WriteLn("\t\t Transfer");
+        if (familyProperties[j].queueFlags & VK_QUEUE_SPARSE_BINDING_BIT) GSLog::WriteLn("\t\t Sparse Binding");
+        }
+}
+
 bool CheckDevices()
 {
     // Query how many devices are present in the system
     uint32_t deviceCount = 0;
 
-    VkResult result = vkEnumeratePhysicalDevices(vk_inst, &deviceCount, NULL);
+    // The first time, we call it to get the number of devices.
+    VkResult result = vkEnumeratePhysicalDevices(context.instance, &deviceCount, NULL);
     if (result != VK_SUCCESS) 
     {
         GSLog::WriteLn("Failed to query the number of physical devices present: %d\n", result);
@@ -82,10 +138,9 @@ bool CheckDevices()
         return false;
     }
 
-    // Get the physical devices
+    // Get the physical devices with the number gotten from the first call.
     std::vector<VkPhysicalDevice> physicalDevices(deviceCount);
-
-    result = vkEnumeratePhysicalDevices(vk_inst, &deviceCount, &physicalDevices[0]);
+    result = vkEnumeratePhysicalDevices(context.instance, &deviceCount, &physicalDevices[0]);
     if (result != VK_SUCCESS) 
     {
         GSLog::WriteLn("Failed to enumerate physical devices present: %d\n", result);
@@ -94,33 +149,51 @@ bool CheckDevices()
 
      GSLog::WriteLn("Number of physical devices present: %d\n", deviceCount);
 
-    // Enumerate all physical devices
-    VkPhysicalDeviceProperties deviceProperties;
-
     for (uint32_t i = 0; i < deviceCount; i++) 
     {
-        memset(&deviceProperties, 0, sizeof deviceProperties);
-        vkGetPhysicalDeviceProperties(physicalDevices[i], &deviceProperties);
-
-        GSLog::WriteLn("Driver Version: %d", deviceProperties.driverVersion);
-        GSLog::WriteLn("Device Name:    %s", deviceProperties.deviceName);
-        GSLog::WriteLn("Device Type:    %d", deviceProperties.deviceType);
-        GSLog::WriteLn("API Version:    %d.%d.%d",
-            // See note below regarding this:
-            VK_VER_MAJOR(deviceProperties.apiVersion),
-            VK_VER_MINOR(deviceProperties.apiVersion),
-            VK_VER_PATCH(deviceProperties.apiVersion));
+        PrintDeviceInfo(physicalDevices[i]);
     }
 
     return true;
 }
 
+bool CheckValidationLayers()
+{
+    uint32_t layerCount = 0;
+    bool foundValidation = false;
+
+    // Check once to get the count.
+    vkEnumerateInstanceLayerProperties( &layerCount, NULL );
+
+    VkLayerProperties *layersAvailable = new VkLayerProperties[layerCount];
+    vkEnumerateInstanceLayerProperties( &layerCount, layersAvailable );
+
+    for (uint32_t i = 0; i < layerCount; ++i) 
+    {
+        if (strcmp( layersAvailable[i].layerName, "VK_LAYER_LUNARG_standard_validation" ) == 0) 
+        {
+            foundValidation = true;
+        }
+    }
+
+    if (!foundValidation)
+    {
+            GSLog::WriteLn("Could not find validation layer.");
+            return false;
+    }
+    
+    GSLog::WriteLn("Found validation layer.");
+    return true;
+}
 void InitVulkan()
 {
     bool ret = false;
+    
     GSLog::WriteLn("Initializing Vulkan! (Sort of.)");
 
-    ret = CreateInstance();
+    ret = CheckValidationLayers();
+
+    ret = CreateInstance(ret); // Create it with validation layers if they are there, otherwise don't use them.
     
     if (ret) ret = CheckDevices();
 
@@ -132,5 +205,5 @@ void InitVulkan()
 
 void DestroyVulkan()
 {
-    vkDestroyInstance(vk_inst, NULL);
+    vkDestroyInstance(context.instance, NULL);
 }
